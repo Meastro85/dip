20eadb138b7d33794ac668e62cdf789c511b136e
Vera Wise <114408064+VW03@users.noreply.github.com>
2025-12-11 17:02:34 +0100

    lineDetection - max 2 lijnen

--- lineDetection.m
+++ lineDetection.m
@@ -1,5 +1,6 @@
 % --- Pre-processing --- %
-d_gray = rgb2gray(d);
+tri_1 = dicomread('BO2WL_F_10089_T1_CALF.dcm');
+d_gray = rgb2gray(tri_1);
 BW = imbinarize(d_gray);
 BW = ~BW;
 edges = edge(BW,'canny',[0.03 0.15]);
@@ -9,6 +10,7 @@ edges = edge(BW,'canny',[0.03 0.15]);
 idx90 = abs(theta - 90) < 5;
 idx_90 = abs(theta - (-90)) < 5;
 idx_horizontal = idx90 | idx_90;
+
 H_only_horizontal = H;
 H_only_horizontal(:, ~idx_horizontal) = 0;
 
@@ -16,29 +18,75 @@ NumPeaks = 10;
 P_all_horz = houghpeaks(H_only_horizontal, NumPeaks, ...
                        'Threshold', 0.2*max(H_only_horizontal(:)), ...
                        'NHoodSize', [11 11]);
-lines_all_horz = houghlines(edges, theta, rho, P_all_horz, 'FillGap', 70, 'MinLength', 100);
+
+lines_all_horz = houghlines(edges, theta, rho, P_all_horz, ...
+                            'FillGap', 70, 'MinLength', 100);
+
+% --- Remove lines near image edges --- %
+imgSize = size(tri_1);
+imgHeight = imgSize(1);
+imgWidth = imgSize(2);
+
+edgeMargin = 40;   % ignore top/bottom border lines
+minSpacing = 50;   % ignored space between lines
+validLines = [];
+
+for i = 1:length(lines_all_horz)
+    y1 = lines_all_horz(i).point1(2);
+    y2 = lines_all_horz(i).point2(2);
+    y_mid = mean([y1, y2]);
+
+    if y_mid > edgeMargin && y_mid < (imgHeight - edgeMargin)
+        validLines = [validLines, lines_all_horz(i)];
+    end
+end
+
+if isempty(validLines)
+    error('No valid horizontal lines detected (all were near the borders).');
+end
+
+
+% Sort by length
+line_lengths = arrayfun(@(L) norm(L.point1 - L.point2), validLines);
+[~, sortedIdx] = sort(line_lengths, 'descend');
+sortedLines = validLines(sortedIdx);
+
+% Pick first (longest)
+selected = sortedLines(1);
+y_first = mean([selected.point1(2), selected.point2(2)]);
+
+% Find second line with enough distance
+secondFound = false;
+for i = 2:length(sortedLines)
+    y_candidate = mean([sortedLines(i).point1(2), sortedLines(i).point2(2)]);
+    if abs(y_candidate - y_first) >= minSpacing
+        second = sortedLines(i);
+        secondFound = true;
+        break;
+    end
+end
+
+if ~secondFound
+    warning('Only one line satisfied spacing requirements. Showing only one.');
+    selectedLines = [selected];
+else
+    selectedLines = [selected, second];
+end
 
 % --- Visualization --- %
 figure;
+imshow(tri_1); hold on;
+title('Horizontal Lines');
 
-subplot(1,2,1); imshow(d); title('Original');
-subplot(1,2,2); imshow(d); hold on;
-title('Selected Horizontal Lines (Longer)');
-lines_to_show_indices = [2, 6];
+for i = 1:length(selectedLines)
+    line_data = selectedLines(i);
 
-selected_colors = [
-    1.0, 0.0, 1.0; % Pink
-    0.5, 0.0, 0.5  % Purple
-];
+    % Forced horizontal: use mean y-position
+    y_level = mean([line_data.point1(2), line_data.point2(2)]);
 
-for i = 1:length(lines_to_show_indices)
-    k = lines_to_show_indices(i);
+    x_full = [1, imgWidth];
+    y_full = [y_level, y_level];
 
-    if k <= length(lines_all_horz)
-        line_data = lines_all_horz(k);
-        xy = [line_data.point1; line_data.point2];
-        plot(xy(:,1), xy(:,2), 'Color', selected_colors(i,:), 'LineWidth', 3);
-    else
-        disp(['Warning: Index ', num2str(k), ' is out of bounds or line did not meet the new length criteria.']);
-    end
+    % Red line
+    plot(x_full, y_full, 'Color', [1, 0, 0], 'LineWidth', 2);
 end
